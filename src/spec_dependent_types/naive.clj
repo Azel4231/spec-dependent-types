dcc185d5d68244abc36724f6bcea044f5418660d
Azel4231 <Azel4231@users.noreply.github.com>
2017-12-27 10:13:59 +0100

    Additional explanations, bump to Clojure 1.9

--- src/spec_dependent_types/naive.clj
+++ src/spec_dependent_types/naive.clj
@@ -27,8 +27,10 @@
 (s/valid? ::dependent-spec [0])
 ;; => true
 
-(s/conform ::dependent-spec [3 "a" "b" "c"])
+(s/conform ::dependent-spec [3 "a" "b" "c"])  
 ;; => [3 "a" "b" "c"]
+;; Conform returns the data unchanged.
+;; This is because in this case order is encoded in a predicate ("#(nat-int? (first %))"), not in the spec itself.
 
 
 
@@ -40,4 +42,29 @@
 
 (do-something [3 "a" "b" "c"])
 ;; => Size:  3 , elements:  (a b c)
+
+
+
+
+
+
+
+;; What happens if the format changes to ["a" "b" "c" 3]?
+#_(defn size-matches? [coll]
+  (= (count coll)
+     (inc (last coll))))   ;; <--- changed to "last"
+
+#_(s/def ::dependent-spec (s/and sequential?
+                               #(nat-int? (last %))   ;; <--- changed to "last"
+                               size-matches?)) 
+
+#_(defn do-something [coll]
+  (when (not (s/valid? ::dependent-spec coll))
+    (throw (RuntimeException.)))
+  (let [count (last coll)             ;; <--- can no longer use destructuring, must use "last"
+        elements (butlast coll)]      ;; <--- must use "butlast"
+    (str "Size: " count ", elements: " elements)))
+
+;; Order dependece is a kind of complexity. It "infects" every usage point.
+;; Sadly in this case both parts of the spec as well as every usage in the code was coupled to that specific order.
 
--- src/spec_dependent_types/nice.clj
+++ src/spec_dependent_types/nice.clj
@@ -18,8 +18,9 @@
 
 (s/conform ::dependent-spec [3 "a" "b" "c"])
 ;; => {:count 3, :elements ["a" "b" "c"]}
-
-
+;; This is a huge difference to the naive version. In this case the order is encoded in the spec itself using "cat".
+;; It enables conform to transform the data into a shape that is order independent (a map).
+;; Which in turn makes the code much less order dependent (see "Bonus question" below).
 
 
 
@@ -50,9 +51,24 @@
 ;; => Size:  3 , elements:  [a b c]
 
 
+
+
 ;; Bonus Question
 ;; -> what happens, when I decide to change the pattern to ["a" "b" "c" 3] ?!
 
-(s/valid? ::dependent-spec ["a" "b" "c" "d" "e" 5])
-;; => true
+;; The only thing that is order dependent is the ::annotated-list spec.
+;; Switching places is really easy compared to figuring out last + butlast in the naive version.
+#_(s/def ::annotated-list (s/cat :elements (s/* any?)   ;; <--- switched places
+                                 :count nat-int? ))     ;; <--- switched places
+
+;; When I wrote this spec I expected to achieve order independence in the usage points (e.g. "do-something").
+;; What suprised me was that even accompanying predicates ("size-matches?") are order independent.
+;; This is due to the fact that the s/and macro passes on conformed data to the next spec:
+                                                         
+#_(s/def ::dependent-spec (s/and ::annotated-list      ;; ["a" "b" "c" 3] is passed in and gets conformed to a map
+                                 size-matches?))       ;; {:count 3, :elements ["a" "b" "c"]} is passed in,
+                                                       ;; which is the same format as before
+
+
+
 
